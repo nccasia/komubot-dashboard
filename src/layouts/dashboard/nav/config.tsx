34b260df0bb4682f405b74c8924d9d3d5feef351
tuanphamminh2 <tuan.phamminh2@ncc.asia>
2023-02-15 13:33:08 +0700

    resolve conflicts

--- src/layouts/dashboard/nav/config.tsx
+++ src/layouts/dashboard/nav/config.tsx
@@ -1,45 +1,55 @@
 // component
-import SvgColor from '../../../components/svg-color';
+import SvgColor from "../../../components/svg-color";
 
 // ----------------------------------------------------------------------
 
-const icon = (name:string) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+const icon = (name: string) => (
+  <SvgColor
+    src={`/assets/icons/navbar/${name}.svg`}
+    sx={{ width: 1, height: 1 }}
+  />
+);
 
 const navConfig = [
   {
-    title: 'dashboard',
-    path: '/dashboard/app',
-    icon: icon('ic_analytics'),
+    title: "dashboard",
+    path: "/dashboard/app",
+    icon: icon("ic_analytics"),
   },
   {
-    title: 'user',
-    path: '/dashboard/user',
-    icon: icon('ic_user'),
+    title: "user",
+    path: "/dashboard/user",
+    icon: icon("ic_user"),
   },
   {
-    title: 'userdiscord',
-    path: '/dashboard/userdiscord',
-    icon: icon('ic_user'),
+    title: "userdiscord",
+    path: "/dashboard/userdiscord",
+    icon: icon("ic_user"),
   },
   {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: icon('ic_cart'),
+    title: "daily",
+    path: "/dashboard/daily",
+    icon: icon("ic_user"),
   },
   {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: icon('ic_blog'),
+    title: "product",
+    path: "/dashboard/products",
+    icon: icon("ic_cart"),
   },
   {
-    title: 'login',
-    path: '/login',
-    icon: icon('ic_lock'),
+    title: "blog",
+    path: "/dashboard/blog",
+    icon: icon("ic_blog"),
   },
   {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic_disabled'),
+    title: "login",
+    path: "/login",
+    icon: icon("ic_lock"),
+  },
+  {
+    title: "Not found",
+    path: "/404",
+    icon: icon("ic_disabled"),
   },
 ];
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -10,6 +10,7 @@ import Page404 from './pages/Page404';
 import ProductsPage from './pages/ProductsPage';
 import DashboardAppPage from './pages/DashboardAppPage';
 import UserDiscord from './pages/UserDiscord';
+import DailyPage from './pages/DailyPage';
 
 // ----------------------------------------------------------------------
 
@@ -25,6 +26,7 @@ export default function Router() {
         { path: 'userdiscord', element: <UserDiscord /> },
         { path: 'products', element: <ProductsPage /> },
         { path: 'blog', element: <BlogPage /> },
+        { path: 'daily', element: <DailyPage /> },
       ],
     },
     {
