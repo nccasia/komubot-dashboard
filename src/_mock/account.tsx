4b4bb9a8b731611ab9ca77d602c2f6440bff6d34
Thế <tienthe2973@gmail.com>
2023-02-21 11:41:45 +0700

    get account

--- src/_mock/account.tsx
+++ src/_mock/account.tsx
@@ -1,7 +1,17 @@
 // ----------------------------------------------------------------------
 
+import jwt from 'jsonwebtoken';
+import { getAccessToken } from '../utils/localStorerage';
+
+// Giả sử token đã được lưu trong state của component
+const token = getAccessToken()
+
+// Giải mã token và lấy thông tin user và email
+const decodedToken = jwt.decode(token);
+const user = decodedToken.username;
+const email = decodedToken.email;
 const account = {
-  displayName: 'Jaydon Frankie',
+  displayName:  localStorage.getItem('username'),
   email: 'demo@minimals.cc',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
